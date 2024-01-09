186c49c47d625e6f0e86e73c0db578ccda2944e0
OmaraRashid <omararashed1@gmail.com>
2024-01-09 23:07:54 +0200

    Add SQL Tasks

---
+++ SQL/GetManagerProjectHours.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE GetManagerProjectHours
+AS
+BEGIN
+    SELECT 
+        Manager.Name AS ManagerName,
+        Project.Name AS ProjectName,
+        SUM(TimeLog.MinutesWorked) AS TotalMinutes
+    FROM TimeLog
+    INNER JOIN Employee ON TimeLog.EmployeeID = Employee.EmployeeID
+    INNER JOIN Project ON TimeLog.ProjectID = Project.ProjectID
+    INNER JOIN Employee AS Manager ON Employee.ManagerID = Manager.EmployeeID
+    GROUP BY Manager.Name, Project.Name
+END
---
+++ SQL/ViewBrowserUsage.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[ViewBrowserUsage]
+AS 
+SELECT ProcessName, SUM(DurationInSec) AS TotalDuration
+FROM ActiveProcessDetails
+WHERE ProcessName IN ('Brave', 'Google Chrome', 'Microsoft Edge', 'Firefox')
+AND TimeStamp BETWEEN '2022-08-08' AND '2022-10-08'
+GROUP BY ProcessName;
